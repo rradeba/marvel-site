04b41be6f4b42a0c6065057e85d6a29bf202358b
rradeba <rradeba@gmail.com>
2024-09-05 10:05:49 -0400

    c1

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TempMain from './TempMain'; // Import your Main component (with the correct case)
+import TempMain from './TempMain'; 
 import './index.css'; // Import global styles if any
 
 // Render the Main component into the root div in index.html
